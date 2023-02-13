6fc1251650a745e02972e378f90b57c8525e3535
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2023-02-13 19:21:59 +0100

    changed icon set

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia';
 import '@quasar/extras/roboto-font/roboto-font.css';
 import '@quasar/extras/material-icons/material-icons.css';
 import '@quasar/extras/fontawesome-v6/fontawesome-v6.css';
-import quasarIconSet from 'quasar/icon-set/fontawesome-v6';
+import quasarIconSet from 'quasar/icon-set/material-icons';
 
 import 'quasar/src/css/index.sass';
 
