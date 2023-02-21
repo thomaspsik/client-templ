6a7f51e637187ddab55ce65612a922f5d068d012
thomaspsik <thomas.psik@htl-ottakring.ac.at>
2023-02-21 18:14:36 +0100

    use store directly without storeToRef function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import { storeToRefs } from 'pinia';
 import { useMyStore } from '../stores/myStore';
 
 const myStore = useMyStore();
-const { message } = storeToRefs(myStore);
 </script>
 
 <template>
-  <div class="column items-center q-mt-md"><span class="text-h4">Home</span><br /><span>{{ message }}</span></div>
+  <div class="column items-center q-mt-md"><span class="text-h4">Home</span><br /><span>{{ myStore.message }}</span></div>
 </template>
