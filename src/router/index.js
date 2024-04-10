635b82b1d0143fff86f410ff993330d5853e931d
Thomas Psik <thomas.psik@htlwienwest.at>
2024-04-10 19:53:58 +0200

    added notfound route

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
       name: 'about',
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/:pathMatch(.*)',
+      name: 'notfound',
+      component: () => import('../views/NotFoundView.vue'),
+    },
   ],
 });
 
